4b4a199a62ba6a1f00eb6fc982a4163377f64cff
Jorge Dias <jorgedsdias@gmail.com>
2019-07-07 12:38:05 -0300

    Alteracoes na lista

--- client/js/models/ListaNegociacoes.js
+++ client/js/models/ListaNegociacoes.js
@@ -1,13 +1,20 @@
 class ListaNegociacoes {
-    constructor() {
+    constructor(trigger) {
         this._negociacoes = [];
-    }
-
-    adiciona(negociacao) {
-        this._negociacoes.push(negociacao);
+        this._trigger = trigger;
     }
 
     get negociacoes() {
         return [].concat(this._negociacoes);
+    }
+
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao);
+        this._trigger(this);
+    }
+
+    esvazia() {
+        this._negociacoes= [];
+        this._trigger(this);
     }
 }
